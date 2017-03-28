5f121576ad4589eaf971dcf88d881c7f73c16b57
WGMI <imo.muganda21@gmail.com>
2017-03-28 01:31:38 -0700

    27/3/17

--- app/src/main/java/com/example/wgmi/expensemanager/Settings.java
+++ app/src/main/java/com/example/wgmi/expensemanager/Settings.java
@@ -1,11 +1,15 @@
 package com.example.wgmi.expensemanager;
 
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -14,18 +18,29 @@ import android.view.ViewGroup;
 public class Settings extends Fragment {
 
     View view;
+    Context context;
+    Dialog dialog;
 
     public Settings() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_settings, container, false);
+        view = inflater.inflate(R.layout.settings, container, false);
+        context = this.getActivity();
+        Button add = (Button) view.findViewById(R.id.add);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog = new Dialog(context);
+                dialog.setContentView(R.layout.new_category_dialog);
+                dialog.show();
+            }
+        });
         return view;
     }
-
 }
